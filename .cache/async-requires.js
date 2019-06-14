9d445401889e07168511375da9920950a9111555
vishal sharma <vishal@innctech.in>
2019-06-14 10:52:56 +0530

    build project

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/opt/lampp/htdocs/shiv-project/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/opt/lampp/htdocs/shiv-project/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("/opt/lampp/htdocs/shiv-project/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-index-js": () => import("/opt/lampp/htdocs/shiv-project/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
---
+++ public/styles-216afed5fdd8d6da2944.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{176:function(n,o,c){},178:function(n,o,c){},179:function(n,o,c){},180:function(n,o,c){},187:function(n,o,c){},188:function(n,o,c){}}]);
+//# sourceMappingURL=styles-216afed5fdd8d6da2944.js.map
